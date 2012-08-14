f924cd16e328e609807bee4e63f1f28deb27c995
Michael Gottesman <mgottesman@apple.com>
2012-08-14 04:21:11 +0000

    [LNT] v4_global_status: Added basic, bare javascript file I am going to file it in over the next commits.
    llvm-svn: 161842

---
+++ lnt/server/ui/static/v4_global_status.js
@@ -0,0 +1,22 @@
+// -*- mode: javascript; -*-
+
+v4_global_status = {};
+(function(_module) {
+    // Assign our input module to a var so that a closure is created
+    // in our local context.
+    var m = _module;
+    
+    /* Globals */
+    var g = {};
+    
+    /* Initialization */
+    $(document).ready(function() {
+        // Create a global variable for table.
+        g.table = $('#data-table')[0];
+    });
+    
+    /* Helper Functions */
+    
+    /* Exported Functions */
+    
+})(v4_global_status);
