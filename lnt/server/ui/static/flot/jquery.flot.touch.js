44709300ded514eccf316984f12510835fc6adc9
Michael Gottesman <mgottesman@apple.com>
2012-09-20 06:25:55 +0000

    [LNT] Added javascript for a simple custom flot html5 touch plugin.
    llvm-svn: 164289

---
+++ lnt/server/ui/static/flot/jquery.flot.touch.js
@@ -0,0 +1,149 @@
+// Based off of jquery.flot.touch.js (which is MIT Licensed), but
+// dumber and using jquery.flot.navigate.js
+
+(function($) {
+    
+    function init(plot) {                        
+        var isOneTouch = false;
+        var isTwoTouch = false;
+        var lastTouchCenter = [0,0];
+        var lastTouchPosition = [0,0];
+        var lastTwoTouchPosition = [[0,0],[0,0]];
+        var lastTwoTouchDistance = Infinity;
+        var lastTouchTime = null;
+        
+        function bindEvents(plot, eventHolder) {
+            if (plot.getOptions().touch.enabled === false)
+                return;
+            
+            var placeholder = plot.getPlaceholder();
+            
+            placeholder.bind('touchstart', function(e) {
+                var touches = e.originalEvent.touches;
+                var offset = plot.offset();
+                
+                if (touches.length === 1) {
+                    // Prepare for either double tap zoom
+                    // or pan.
+                    isOneTouch = true;
+                    
+                    lastTouchPosition = [touches[0].pageX,
+                                         touches[0].pageY];
+                    lastTouchCenter = [touches[0].pageX - offset.left,
+                                       touches[0].pageY - offset.top];
+                } else if (touches.length === 2) {
+                    // Prepare for pinch zoom.           
+                    isTwoTouch = true;
+                    lastTwoTouchPosition = [[touches[0].pageX,
+                                             touches[0].pageY],
+                                            [touches[1].pageX,
+                                             touches[1].pageY]];
+                    lastTouchCenter = [(touches[1].pageX + touches[0].pageX)/2 - offset.left,
+                                       (touches[1].pageY + touches[0].pageY)/2 - offset.top];
+                    
+                    var xdelta = touches[1].pageX - touches[0].pageX;
+                    var ydelta = touches[1].pageY - touches[0].pageY;
+                    lastTwoTouchDistance = Math.sqrt(xdelta*xdelta + ydelta*ydelta);
+                }
+                return false;
+            });
+            
+            placeholder.bind('touchmove', function(e) {
+                var touches = e.originalEvent.touches;
+                
+                if (isOneTouch && touches.length === 1) {
+                    // If we hit a touchmove with one touch,
+                    // we are panning.
+                    var newTouchPosition = [touches[0].pageX,
+                                            touches[0].pageY];
+                    plot.pan({
+                        left: lastTouchPosition[0] - newTouchPosition[0],
+                        top: lastTouchPosition[1] - newTouchPosition[1]
+                    });
+                    
+                    lastTouchPosition = newTouchPosition;
+                } else if (isTwoTouch && touches.length === 2) {
+                    // If we hit a touchmove with one touch,
+                    // we are zooming.
+                    
+                    // We look at the delta from our last positions and zoom
+                    // in by the percent difference from the total distance in between
+                    // the previous distance in between the fingers total.                    
+                    var xdelta = touches[1].pageX - touches[0].pageX;
+                    var ydelta = touches[1].pageY - touches[0].pageY;
+                    var newTwoTouchDistance = Math.sqrt(xdelta*xdelta + ydelta*ydelta);
+                    var scale = 1.0 + (newTwoTouchDistance - lastTwoTouchDistance)/lastTwoTouchDistance;
+                    
+                    plot.zoom({ amount: scale, center: { left: lastTouchCenter[0], top: lastTouchCenter[1] }});
+                    
+                    lastTwoTouchDistance = newTwoTouchDistance;
+                }
+                return false;
+            });
+            
+            placeholder.bind('touchend', function(e) {
+                var touches = e.originalEvent.touches;
+                
+                // Do the pan and or double click if it was quick.
+                if (isOneTouch) {
+                    var now = new Date().getTime();
+                    var lasttime = lastTouchTime || now + 1; // now + 1 so the first time we are negative.
+                    var delta = now - lasttime;
+                    
+                    if (delta < 500 && delta > 0) {
+                        // We have a double touch.
+                        plot.zoom({ center: { left: lastTouchCenter[0],
+                                              top: lastTouchCenter[1] }});
+                    }
+                    
+                    lastTouchTime = lastTouchTime !== null? null : now;
+                } else if (isTwoTouch) {
+                    // Finish off our zoom.
+
+                    // We look at the delta from our last positions and zoom
+                    // in by the percent difference from the total distance in between
+                    // the previous distance in between the fingers total.                    
+                    var xdelta = touches[1].pageX - touches[0].pageX;
+                    var ydelta = touches[1].pageY - touches[0].pageY;
+                    var newTwoTouchDistance = Math.sqrt(xdelta*xdelta + ydelta*ydelta);
+                    var scale = (newTwoTouchDistance - lastTwoTouchDistance)/lastTwoTouchDistance;
+                    lastTwoTouchDistance = newTwoTouchDistance;
+                    
+                    plot.zoom({ amount: scale,
+                                center: { left: lastTouchCenter[0], top: lastTouchCenter[1] }});
+                }
+                
+                isOneTouch = false;
+                isTwoTouch = false;
+                return false;
+            });
+        }
+        
+        function shutdown(plot, eventHolder) {
+            if (plot.getOptions().touch.enabled === false)
+                return;
+            var placeholder = plot.getPlaceholder();
+            placeholder.unbind('touchstart').unbind('touchmove').unbind('touchend');
+        }
+        
+        plot.hooks.bindEvents.push(bindEvents);
+        plot.hooks.shutdown.push(shutdown);
+        $(document).bind('ready orientationchange', function(e) {
+	    window.scrollTo(0, 1);
+	    
+	    setTimeout(function() {
+		$.plot(placeholder, plot.getData(), plot.getOptions());
+	    }, 50);
+	});
+    }
+    
+    var options = {
+        touch: {enabled: true}
+    };
+    $.plot.plugins.push({
+        init: init,
+        options: options,
+        name: 'touch',
+        version: '1.0'
+    });
+})(jQuery);
