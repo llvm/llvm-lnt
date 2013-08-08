db174bf761b9d8121da2ddf2242b05700fdc5033
Chris Matthews <cmatthews5@apple.com>
2013-08-08 23:42:58 +0000

    Update the runtest lit test cases to cover two extra code paths, --multisample and --only-test.
    llvm-svn: 188026

---
+++ tests/runtest/multisample.py
@@ -0,0 +1,32 @@
+# Testing for the 'lnt runtest nt' module.
+#
+# RUN: lnt runtest nt \
+# RUN:   --sandbox %t.SANDBOX \
+# RUN:   --test-suite %S/Inputs/test-suite \
+# RUN:   --cc %{shared_inputs}/FakeCompilers/clang-r154331 \
+# RUN:   --no-timestamp --multisample 2 > %t.log 2> %t.err
+#
+# RUN: FileCheck --check-prefix CHECK-STDOUT < %t.log %s
+# RUN: FileCheck --check-prefix CHECK-STDERR < %t.err %s
+#
+# CHECK-STDOUT: Import succeeded.
+# CHECK-STDOUT: Added Machines: 1
+# CHECK-STDOUT: Added Runs    : 1
+# CHECK-STDOUT: Added Tests   : 130
+# CHECK-STDOUT: --- Tested: 260 tests --
+
+# CHECK-STDERR: inferred C++ compiler under test
+# CHECK-STDERR: (multisample) running iteration 0
+# CHECK-STDERR: checking source versions
+# CHECK-STDERR: using nickname
+# CHECK-STDERR: starting test
+# CHECK-STDERR: configuring
+# CHECK-STDERR: building test-suite tools
+# CHECK-STDERR: executing "nightly tests" with -j1
+# CHECK-STDERR: loading nightly test data
+# CHECK-STDERR: capturing machine information
+# CHECK-STDERR: generating report
+# CHECK-STDERR: (multisample) running iteration 1
+# CHECK-STDERR: submitting result to dummy instance
+
+
