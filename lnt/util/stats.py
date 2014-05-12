Fix Mann-Whitney U calculation with large sample size
SciPy uses one-sided p-value while rest of program uses two-sided, multiplied
by 2.

llvm-svn: 208556

lnt/util/stats.py
@@ -31,7 +31,9 @@ def mannwhitneyu(a, b, sigLevel = .05):
     else:
         try:
             from scipy.stats import mannwhitneyu as mannwhitneyu_large
-            return mannwhitneyu_large(a, b, False) >= sigLevel
+            # MWU in SciPy is one-sided, multiply by 2 to get two-sided.
+            p = mannwhitneyu_large(a, b, False) * 2
+            return p >= sigLevel
         except ValueError:
             return True
 