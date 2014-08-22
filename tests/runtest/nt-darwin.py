3812e2958db8d38cde9610340060a6c950249cf4
Yi Kong <Yi.Kong@arm.com>
2014-08-22 21:48:41 +0000

    Make sure lit tests pass on non-Darwin systems
    lit tests always fail on non-Darwin systems because --use-isolation uses
    utilities that are only available on OS X.
    
    This patch moves the test to an Darwin-only test case, so that unit tests can
    be ran by Linux buildbots.
    
    llvm-svn: 216290

---
+++ tests/runtest/nt-darwin.py
@@ -0,0 +1,13 @@
+# Darwin specific testing for the 'lnt runtest nt' module.
+
+# Run with sandboxing enabled.
+# RUN: lnt runtest nt \
+# RUN:   --sandbox %t.SANDBOX \
+# RUN:   --test-suite %S/Inputs/test-suite \
+# RUN:   --cc %{shared_inputs}/FakeCompilers/clang-r154331 \
+# RUN:   --no-timestamp --use-isolation > %t.log 2> %t.err
+# RUN: FileCheck --check-prefix CHECK-SANDBOX < %t.err %s
+#
+# CHECK-SANDBOX: creating sandbox profile
+
+# REQUIRES: Darwin
