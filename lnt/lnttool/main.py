llvm/llvm-lnt

commit be225f6240e3a4cf8943ad9686dc1dd6735ae4bf

Do not fail on missing result_url when submitting builds
This should fix our failing buildbots. Not sure this is indeed the
optimal solution.

llvm-svn: 237540

lnt/lnttool/main.py
@@ -153,7 +153,10 @@ def action_runtest(name, args):
         parser.error('invalid test name %r' % test_name)
 
     server_results = test_instance.run_test('%s %s' % (name, test_name), args)
-    print "Results available at:", server_results['result_url']
+    if result.get('result_url'):
+        print "Results available at:", server_results['result_url']
+    else:
+        print "Results available at: no URL available"
 
 
 def action_showtests(name, args):

commit 1770aac2725e3f402ebaf4b438f7896a0562031b

Fix bug in buildbot reporting introduced in r237540
llvm-svn: 237564

lnt/lnttool/main.py
@@ -153,7 +153,7 @@ def action_runtest(name, args):
         parser.error('invalid test name %r' % test_name)
 
     server_results = test_instance.run_test('%s %s' % (name, test_name), args)
-    if result.get('result_url'):
+    if server_results.get('result_url'):
         print "Results available at:", server_results['result_url']
     else:
         print "Results available at: no URL available"