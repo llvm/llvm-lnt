00cf4c87315229adde4742eb34047b0049333bac
James Molloy <james.molloy@arm.com>
2016-03-21 10:46:28 +0000

    [profile] Hook LinuxPerfProfile into the implementation registry
    This allows the importer to discover it and import linux perf data.
    
    llvm-svn: 263927

--- lnt/testing/profile/__init__.py
+++ lnt/testing/profile/__init__.py
@@ -2,4 +2,5 @@
 
 from profilev1impl import ProfileV1
 from profilev2impl import ProfileV2
-IMPLEMENTATIONS = {1: ProfileV1, 2: ProfileV2}
+from perf import LinuxPerfProfile
+IMPLEMENTATIONS = {0: LinuxPerfProfile, 1: ProfileV1, 2: ProfileV2}
