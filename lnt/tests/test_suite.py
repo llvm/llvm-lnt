986a5ff746d2b302264cfc3b8204a211afd0671e
Matthias Braun <matze@braunis.de>
2017-03-07 23:59:21 +0000

    Fix typo from r297125
    llvm-svn: 297245

--- lnt/tests/test_suite.py
+++ lnt/tests/test_suite.py
@@ -595,7 +595,7 @@ class TestSuiteTest(BuiltinTest):
         if self.opts.cc:
             defs['CMAKE_C_COMPILER'] = self.opts.cc
         if self.opts.cxx:
-            defs['CMAKE_CXX_COMPILER'] = self.opts.cxx,
+            defs['CMAKE_CXX_COMPILER'] = self.opts.cxx
 
         if self.opts.cppflags or self.opts.cflags:
             all_cflags = ' '.join([self.opts.cppflags, self.opts.cflags])
