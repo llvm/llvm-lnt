b65c6b32023a816dee4758d663aa8c2b5c404b82
Matthias Braun <matze@braunis.de>
2017-07-06 22:06:30 +0000

    Remove unused detectCPUs function
    It wasn't used, also we use python 2.7 now so we have
    multiprocessing.cpu_count().
    
    llvm-svn: 307325

--- lnt/server/ui/util.py
+++ lnt/server/ui/util.py
@@ -11,28 +11,6 @@ def toColorString(col):
     return "#%02x%02x%02x" % (r, g, b)
 
 
-def detectCPUs():
-    """
-    Detects the number of CPUs on a system. Cribbed from pp.
-    """
-    import os
-    # Linux, Unix and MacOS:
-    if hasattr(os, "sysconf"):
-        if os.sysconf_names.has_key("SC_NPROCESSORS_ONLN"):
-            # Linux & Unix:
-            ncpus = os.sysconf("SC_NPROCESSORS_ONLN")
-            if isinstance(ncpus, int) and ncpus > 0:
-                return ncpus
-        else:  # OSX:
-            return int(os.popen2("sysctl -n hw.ncpu")[1].read())
-    # Windows:
-    if os.environ.has_key("NUMBER_OF_PROCESSORS"):
-        ncpus = int(os.environ["NUMBER_OF_PROCESSORS"]);
-        if ncpus > 0:
-            return ncpus
-        return 1  # Default
-
-
 def pairs(list):
     return zip(list[:-1], list[1:])
 
