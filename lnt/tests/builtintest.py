2508ccb836df3b95765a0cead689e7055702e7e4
Chris Matthews <cmatthews5@apple.com>
2017-08-18 21:43:48 +0000

    If a server error has happened, don't try and print the server report
    llvm-svn: 311203

--- lnt/tests/builtintest.py
+++ lnt/tests/builtintest.py
@@ -79,9 +79,9 @@ class BuiltinTest(object):
                 updateMachine=config.update_machine, mergeRun=config.merge)
         else:
             server_report = lnt.util.ImportData.no_submit()
-
-        ImportData.print_report_result(server_report, sys.stdout, sys.stderr,
-                                       config.verbose)
+        if server_report:
+            ImportData.print_report_result(server_report, sys.stdout, sys.stderr,
+                                           config.verbose)
         return server_report
 
     @staticmethod
