ac2f702ccca1e0d2399ef65ed0d47d0cd48b5058
Chris Matthews <cmatthews5@apple.com>
2017-08-22 22:33:09 +0000

    Fix postgres_wrapper.sh
    Turns out this is not run with the default tests, so I was not even
    running this locally. It failed badly in CI. Now I really ran it, and
    fixed the issues.
    
    llvm-svn: 311506

--- tests/SharedInputs/postgres_wrapper.sh
+++ tests/SharedInputs/postgres_wrapper.sh
@@ -8,33 +8,34 @@
 #
 # Inspired by https://github.com/tk0miya/testing.postgresql
 set -u
-
-DBDIR="$(mktemp -d -t lnt)"
+TEST_DIR=$1
 shift
-if [ -d "${DBDIR}" ]; then
-    echo 1>&2 "${DBDIR} already exists"
+DB_DIR="$(mktemp -d -t lnt)"
+if [ -d "${TEST_DIR}" ]; then
+    echo 1>&2 "${TEST_DIR} already exists"
     exit 1
 fi
 
-mkdir -p "${DBDIR}"
+mkdir -p "${TEST_DIR}"
+ln -s ${TEST_DIR}/db_root ${DB_DIR}
 
-INITDB_FLAGS+=" --pgdata=${DBDIR}/db"
-INITDB_FLAGS+=" --xlogdir=${DBDIR}/db"
+INITDB_FLAGS+=" --pgdata=${DB_DIR}/db"
+INITDB_FLAGS+=" --xlogdir=${DB_DIR}/db"
 INITDB_FLAGS+=" --nosync"
 INITDB_FLAGS+=" --no-locale"
 INITDB_FLAGS+=" --auth=trust"
 INITDB_FLAGS+=" --username=pgtest"
-echo "$ initdb $INITDB_FLAGS >& ${DBDIR}/initdb_log.txt"
-initdb ${INITDB_FLAGS} >& ${DBDIR}/initdb_log.txt
+echo "$ initdb $INITDB_FLAGS >& ${DB_DIR}/initdb_log.txt"
+initdb ${INITDB_FLAGS} >& ${DB_DIR}/initdb_log.txt
 
 POSTGRES_FLAGS+=" -p 9100"
-POSTGRES_FLAGS+=" -D ${DBDIR}/db"
-POSTGRES_FLAGS+=" -k ${DBDIR}/db"
+POSTGRES_FLAGS+=" -D ${DB_DIR}/db"
+POSTGRES_FLAGS+=" -k ${DB_DIR}/db"
 POSTGRES_FLAGS+=" -h 127.0.0.1"
 POSTGRES_FLAGS+=" -F"
 POSTGRES_FLAGS+=" -c logging_collector=off"
-echo "$ postgres $POSTGRES_FLAGS >& ${DBDIR}/server_log.txt"
-postgres ${POSTGRES_FLAGS} >& ${DBDIR}/server_log.txt &
+echo "$ postgres $POSTGRES_FLAGS >& ${DB_DIR}/server_log.txt"
+postgres ${POSTGRES_FLAGS} >& ${DB_DIR}/server_log.txt &
 PG_PID=$!
 sleep 1 # Give the server time to start.
 
@@ -44,7 +45,8 @@ RC=$?
 
 # Kill server
 kill -15 ${PG_PID}
-[ $? -ne 0 ] && (echo 1>&1 "Could not kill postgres server"; exit 1)
+[ $? -ne 0 ] && (echo 1>&1 "Error: Could not kill postgres server"; exit 1)
 wait ${PG_PID}
-rm -rf ${DBDIR}
+[ ${RC} -ne 0 ] && (rm -rf ${DB_DIR})
 exit ${RC}
+
