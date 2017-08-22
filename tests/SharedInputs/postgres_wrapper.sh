4f00fe65df118a1dc198b5400efeae94ffc812ca
Chris Matthews <cmatthews5@apple.com>
2017-08-22 22:01:30 +0000

    Switch away from shell simple variables
    llvm-svn: 311502

--- tests/SharedInputs/postgres_wrapper.sh
+++ tests/SharedInputs/postgres_wrapper.sh
@@ -18,23 +18,23 @@ fi
 
 mkdir -p "${DBDIR}"
 
-INITDB_FLAGS+=" --pgdata=$DBDIR/db"
-INITDB_FLAGS+=" --xlogdir=$DBDIR/db"
+INITDB_FLAGS+=" --pgdata=${DBDIR}/db"
+INITDB_FLAGS+=" --xlogdir=${DBDIR}/db"
 INITDB_FLAGS+=" --nosync"
 INITDB_FLAGS+=" --no-locale"
 INITDB_FLAGS+=" --auth=trust"
 INITDB_FLAGS+=" --username=pgtest"
-echo "$ initdb $INITDB_FLAGS >& $DBDIR/initdb_log.txt"
-initdb $INITDB_FLAGS >& $DBDIR/initdb_log.txt
+echo "$ initdb $INITDB_FLAGS >& ${DBDIR}/initdb_log.txt"
+initdb ${INITDB_FLAGS} >& ${DBDIR}/initdb_log.txt
 
 POSTGRES_FLAGS+=" -p 9100"
-POSTGRES_FLAGS+=" -D $DBDIR/db"
-POSTGRES_FLAGS+=" -k $DBDIR/db"
+POSTGRES_FLAGS+=" -D ${DBDIR}/db"
+POSTGRES_FLAGS+=" -k ${DBDIR}/db"
 POSTGRES_FLAGS+=" -h 127.0.0.1"
 POSTGRES_FLAGS+=" -F"
 POSTGRES_FLAGS+=" -c logging_collector=off"
-echo "$ postgres $POSTGRES_FLAGS >& $DBDIR/server_log.txt"
-postgres $POSTGRES_FLAGS >& $DBDIR/server_log.txt &
+echo "$ postgres $POSTGRES_FLAGS >& ${DBDIR}/server_log.txt"
+postgres ${POSTGRES_FLAGS} >& ${DBDIR}/server_log.txt &
 PG_PID=$!
 sleep 1 # Give the server time to start.
 
@@ -43,8 +43,8 @@ eval "$@"
 RC=$?
 
 # Kill server
-kill -15 $PG_PID
+kill -15 ${PG_PID}
 [ $? -ne 0 ] && (echo 1>&1 "Could not kill postgres server"; exit 1)
-wait $PG_PID
-exit $RC
+wait ${PG_PID}
 rm -rf ${DBDIR}
+exit ${RC}
