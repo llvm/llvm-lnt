5669f6609b4b5c548ba666570b0563cd858a93c8
Chris Matthews <cmatthews5@apple.com>
2018-02-13 23:36:56 +0000

    Some cleanups of unused
    llvm-svn: 325078

--- lnt/server/ui/views.py
+++ lnt/server/ui/views.py
@@ -1092,12 +1092,11 @@ def v4_graph():
         # Compute the moving average and or moving median of our data if
         # requested.
         if moving_average or moving_median:
-            fun = None
 
-            def compute_moving_average(x, window, average_list, median_list):
+            def compute_moving_average(x, window, average_list, _):
                 average_list.append((x, lnt.util.stats.mean(window)))
 
-            def compute_moving_median(x, window, average_list, median_list):
+            def compute_moving_median(x, window, _, median_list):
                 median_list.append((x, lnt.util.stats.median(window)))
 
             def compute_moving_average_and_median(x, window, average_list,
@@ -1435,7 +1434,6 @@ def v4_daily_report(year, month, day):
 
     filter_machine_regex = request.args.get('filter-machine-regex')
 
-    session = request.session
     ts = request.get_testsuite()
 
     # Create the report object.
