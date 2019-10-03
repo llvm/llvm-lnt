398d797e02d0590c67fa85a982e3746c08ea8e0c
Thomas Preud'homme <thomasp@graphcore.ai>
2019-10-03 15:18:06 +0000

    [LNT] Python 3 support: fix convert to JSON
    Plistlib and json modules have conflicting requirements for their dump
    methods: Plistlib expects its file handle to be a writable binary file
    object while json expects it to be a writable text file object.
    
    This commit solves this issue by dumping the JSON into a temporary
    string which is encoded to UTF-8 (JSON format RFC 7159 requires UTF-8,
    UTF-16 or UTF-32 encoding and UTF-8 is the recommended default) before
    being output. This allows to keep delegating file opening to Click and
    avoid a refactoring to abstract the opening mode between supported
    formats.
    
    Reviewers: cmatthews, hubert.reinterpretcast, kristof.beyls
    
    Reviewed By: hubert.reinterpretcast
    
    Subscribers: llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D68223
    
    llvm-svn: 373615

--- lnt/formats/JSONFormat.py
+++ lnt/formats/JSONFormat.py
@@ -19,9 +19,17 @@ def _load_format(path_or_file):
     return json.load(path_or_file)
 
 
+def _dump_format(obj, fp):
+    # The json module produces str objects but fp is opened in binary mode
+    # (since Plistlib only dump to binary mode files) so we first dump into
+    # a string a convert to UTF-8 before outputing.
+    json_str = json.dumps(obj)
+    fp.write(json_str.encode())
+
+
 format = {
     'name': 'json',
     'predicate': _matches_format,
     'read': _load_format,
-    'write': json.dump,
+    'write': _dump_format,
 }
