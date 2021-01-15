llvm/llvm-lnt

commit 042938b5c332fa63f1fefdd7116f39e161b8c51b

Fix __all__ mypy warnings
mypy warns about the unknown type for empty __all__. This commit adds
type info for them. Import errors for typing module in LNT needs to be
ignored because LNT module is imported from setup before all required
package are guaranteed to be installed.

Reviewed By: PrzemekWirkus

Differential Revision: https://reviews.llvm.org/D94666

lnt/__init__.py
@@ -3,4 +3,11 @@ __email__ = 'daniel@zuster.org'
 __versioninfo__ = (0, 4, 2)
 __version__ = '.'.join(map(str, __versioninfo__)) + '.dev0'
 
-__all__ = []
+# lnt module is imported from the setup script before modules are installed so
+# modules might not be available.
+try:
+    from typing import Sequence
+except Exception:
+    pass
+
+__all__ = [] # type: Sequence[str]

lnt/server/__init__.py
@@ -1,1 +1,3 @@
-__all__ = []
+from typing import Sequence
+
+__all__ = [] # type: Sequence[str]

lnt/server/db/__init__.py
@@ -1,1 +1,3 @@
-__all__ = []
+from typing import Sequence
+
+__all__ = [] # type: Sequence[str]

lnt/server/db/rules/__init__.py
@@ -1,1 +1,3 @@
-__all__ = []
+from typing import Sequence
+
+__all__ = [] # type: Sequence[str]

lnt/server/reporting/__init__.py
@@ -1,1 +1,3 @@
-__all__ = []
+from typing import Sequence
+
+__all__ = [] # type: Sequence[str]

lnt/server/ui/__init__.py
@@ -1,1 +1,3 @@
-__all__ = []
+from typing import Sequence
+
+__all__ = [] # type: Sequence[str]

lnt/util/__init__.py
@@ -1,5 +1,6 @@
 import logging
+from typing import Sequence
 
 logger = logging.getLogger("lnt.server.ui.app")
 
-__all__ = []
+__all__ = [] # type: Sequence[str]

lnt/testing/util/__init__.py
@@ -2,4 +2,6 @@
 Miscellaneous utilities for generating test data.
 """
 
-__all__ = []
+from typing import Sequence
+
+__all__ = [] # type: Sequence[str]

commit 99b21a5b0bfea1e8b1828692404b56985e89a845

Fix space before typing comments
Fix Flake8's E261 error (at least two spaces before inline comments)
introduced by earlier 042938b5c332fa63f1fefdd7116f39e161b8c51b commit.

Reviewed By: tnfchris

Differential Revision: https://reviews.llvm.org/D94790

lnt/__init__.py
@@ -10,4 +10,4 @@ try:
 except Exception:
     pass
 
-__all__ = [] # type: Sequence[str]
+__all__ = []  # type: Sequence[str]

lnt/server/__init__.py
@@ -1,3 +1,3 @@
 from typing import Sequence
 
-__all__ = [] # type: Sequence[str]
+__all__ = []  # type: Sequence[str]

lnt/server/db/__init__.py
@@ -1,3 +1,3 @@
 from typing import Sequence
 
-__all__ = [] # type: Sequence[str]
+__all__ = []  # type: Sequence[str]

lnt/server/db/rules/__init__.py
@@ -1,3 +1,3 @@
 from typing import Sequence
 
-__all__ = [] # type: Sequence[str]
+__all__ = []  # type: Sequence[str]

lnt/server/reporting/__init__.py
@@ -1,3 +1,3 @@
 from typing import Sequence
 
-__all__ = [] # type: Sequence[str]
+__all__ = []  # type: Sequence[str]

lnt/server/ui/__init__.py
@@ -1,3 +1,3 @@
 from typing import Sequence
 
-__all__ = [] # type: Sequence[str]
+__all__ = []  # type: Sequence[str]

lnt/util/__init__.py
@@ -3,4 +3,4 @@ from typing import Sequence
 
 logger = logging.getLogger("lnt.server.ui.app")
 
-__all__ = [] # type: Sequence[str]
+__all__ = []  # type: Sequence[str]

lnt/testing/util/__init__.py
@@ -4,4 +4,4 @@ Miscellaneous utilities for generating test data.
 
 from typing import Sequence
 
-__all__ = [] # type: Sequence[str]
+__all__ = []  # type: Sequence[str]