87eb0317d4e495863e865d701f099a0ec5b634be
Pavel Kosov <kosov.pavel@huawei.com>
2021-10-20 18:32:02 +0300

    Fixed plistlib API usage
    plistlib API required the file object opened in the binary mode.
    Test command: lnt checkformat tests/SharedInputs/sample-a-small.plist
    Related to: https://reviews.llvm.org/D109235
    
    Reviewed By: thopre
    
    Differential Revision: https://reviews.llvm.org/D112136
    
    Huawei Russian Research Institute, Saint-Petersburg

--- lnt/formats/PlistFormat.py
+++ lnt/formats/PlistFormat.py
@@ -3,15 +3,27 @@ import plistlib
 
 def _matches_format(path_or_file):
     try:
-        plistlib.load(path_or_file)
+        if isinstance(path_or_file, str):
+            with open(path_or_file, 'rb') as fp:
+                plistlib.load(fp)
+        else:
+            plistlib.load(path_or_file)
         return True
     except Exception:
         return False
 
 
+def _load_format(path_or_file):
+    if isinstance(path_or_file, str):
+        with open(path_or_file, 'rb') as fp:
+            return plistlib.load(fp)
+    else:
+        return plistlib.load(path_or_file)
+
+
 format = {
     'name': 'plist',
     'predicate': _matches_format,
-    'read': plistlib.load,
+    'read': _load_format,
     'write': plistlib.dump,
 }
