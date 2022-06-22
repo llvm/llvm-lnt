a980860b1654de37c9a32c74c1fff9b23cce4feb
Chris Matthews <chris.matthews@apple.com>
2022-06-22 15:23:24 -0700

    Multi-table support for Tableau
    Run info and samples are extracted as separate tables.
    
    Each is inserted into Tableau batched.
    
    This makes the extracts much smaller and faster.

--- lnt/server/ui/static/lnt_tableau.js
+++ lnt/server/ui/static/lnt_tableau.js
@@ -64,107 +64,196 @@
     var field_info = getValue(ts_url + "/fields/");
 
     var fields = field_info.fields;
-    var cols = [];
-    cols.push({
+    var sample_cols = [];
+    var run_cols = [];
+
+    run_cols.push({
         id: "machine_name",
         alias: "Machine Name",
         dataType: tableau.dataTypeEnum.string
-      });
-      cols.push({
-        id: "run_id",
-        alias: "Run ID",
-        dataType: tableau.dataTypeEnum.int
-      });
-      cols.push({
-        id: "run_order",
-        alias: "Run Order",
-        dataType: tableau.dataTypeEnum.string
-      });
-      cols.push({
-        id: "run_date",
-        alias: "Run DateTime",
-        dataType: tableau.dataTypeEnum.datetime
-      });
-      cols.push({
-        id: "test_name",
-        alias: "Test",
-        dataType: tableau.dataTypeEnum.string
-      });
+    });
 
-      fields.forEach(function(field) {
-        cols.push({
-          id: field.column_name,
-          alias: field.column_name,
-          dataType: col_type_mapper[field.column_name]
-        });
+    run_cols.push({
+      id: "run_id",
+      alias: "Run ID",
+      dataType: tableau.dataTypeEnum.int
+    });
+    sample_cols.push({
+      id: "run_id",
+      alias: "Run ID",
+      dataType: tableau.dataTypeEnum.int,
+      filterable: true,
+      foreignKey: {
+        "tableId": "measurement_data",
+        "columnId": "run_id"
+      }
+    });
+    run_cols.push({
+      id: "run_order",
+      alias: "Run Order",
+      dataType: tableau.dataTypeEnum.string
+    });
+    run_cols.push({
+      id: "run_date",
+      alias: "Run DateTime",
+      dataType: tableau.dataTypeEnum.datetime
+    });
+    sample_cols.push({
+      id: "test_name",
+      alias: "Test",
+      dataType: tableau.dataTypeEnum.string
+    });
+
+    fields.forEach(function(field) {
+      sample_cols.push({
+        id: field.column_name,
+        alias: field.column_name,
+        dataType: col_type_mapper[field.column_name]
       });
-      var tableSchema = {
-        id: "lnt_machine_feed",
-        alias: "Performance Data from " + field_info.generated_by,
-        columns: cols,
+    });
+
+      var measurementsSchema = {
+        id: "measurement_data",
+        alias: "Measurement Data",
+        columns: sample_cols,
+        incrementColumnId: "run_id",
+        joinOnly: true
+      };
+
+      var runSchema = {
+        id: "run_information",
+        alias: "Run Information",
+        columns: run_cols,
         incrementColumnId: "run_id"
+      };
+
+    var standardConnection = {
+      "alias": "Measurements with Run Info",
+      "tables": [{
+        "id": "run_information",
+        "alias": "Run Information"
+      }, {
+        "id": "measurement_data",
+        "alias": "Measurements"
+      }],
+      "joins": [{
+        "left": {
+          "tableAlias": "Run Information",
+          "columnId": "run_id"
+        },
+        "right": {
+          "tableAlias": "Measurements",
+          "columnId": "run_id"
+        },
+        "joinType": "inner"
+      }]
     };
-    schemaCallback([tableSchema]);
+    //
+    schemaCallback([runSchema, measurementsSchema],[standardConnection] );
   }
 
 
   // Download the data.
   myConnector.getData = function (table, doneCallback) {
+    // How often (in terms of fetches) to report progress.
+    var progress_batch_size = 100;
     var last_run_id = parseInt(table.incrementValue || 0);
+    if (table.tableInfo.id === "run_information") {
+      // Collect the Run Infos table.
+      // Get latest machines.
+      var search_info = JSON.parse(tableau.connectionData);
+      var machine_names = get_matching_machines(search_info.machine_regexp);
+      if (machine_names.length === 0) {
+        tableau.abortWithError("Did not match any machine names matching: " +
+            search_info.machine_regexp);
+      } else {
+         tableau.reportProgress("Found " + machine_names.length +
+             " machines to fetch.");
+      }
 
-    // Get latest machines.
-    var search_info = JSON.parse(tableau.connectionData);
-    var machine_names = get_matching_machines(search_info.machine_regexp);
-    if (machine_names.length === 0) {
-      tableau.abortWithError("Did not match any machine names matching: " +
-          search_info.machine_regexp);
-    } else {
-       tableau.reportProgress("Found " + machine_names.length +
-           " machines to fetch.");
-    }
-
-    machine_names.forEach(function (machine) {
-      var url = ts_url + "/machines/" + machine.id;
-      var machine_info = getValue(url);
-      var machine_name = machine_info.machine.name;
+      // Get runs for each of the filtered machines in batches.
       var tableData = [];
+      // It is faster to submit in batches.
+      var submission_batch_size = 10000;
+      var total_fetched = 1;
+      machine_names.forEach(function (machine) {
+        var machines_run_data = getValue(ts_url + "/machines/" + machine.id)
 
-      machine_info.runs.forEach(function(run, index) {
-        var run_data;
-        var runs_total = machine_info.runs.length;
-        // Run based incremental refresh. If we have already seen data, skip it.
-        if (run.id <= last_run_id) {
-          return;
+        machines_run_data.runs.forEach(function(run_info){
+          // Incremental support.
+          if (run_info.id <= last_run_id) {
+              return;
+          }
+
+          var date_str = run_info.end_time;
+          var run_date = new Date(date_str);
+          tableData.push({run_id: run_info.id,
+            machine_name: machines_run_data.machine.name,
+            run_order: run_info[run_info.order_by],
+            run_date: run_date});
+
+          if (total_fetched % submission_batch_size == 0) {
+            table.appendRows(tableData);
+            tableData.length = 0;  // Drop the submitted rows.
+          }
+          total_fetched = total_fetched + 1;
+        });
+      });
+      table.appendRows(tableData);
+    } else if (table.tableInfo.id === "measurement_data") {
+      // Collect Sample data.
+      var filterValues = table.filterValues;
+
+      if (!table.isJoinFiltered) {
+        tableau.abortWithError("The table must be filtered first.");
+        return;
+      }
+
+      if (filterValues.length === 0) {
+        doneCallback();
+        return;
+      }
+
+      var tableData = [];
+      var total_fetched = 1;
+      // Processing by batch is much faster; however, there is a
+      // 128mb limit to the JS interpreter in Tableau.
+      // This is a guess with some envelope math.
+      var submission_batch_size = 10000;
+
+      for (var i in filterValues) {
+
+        var run_id = filterValues[i];
+        if (run_id <= last_run_id) {
+              return;
         }
 
-        var status_msg = "Getting Machine: " + machine_name
-           + " Run: " + run.id
-           + " (" + (index + 1) + "/" + runs_total + ")";
+        if (total_fetched % progress_batch_size == 1) {
+          var next_run_max = total_fetched + progress_batch_size;
+          var status_msg = "Getting Runs: " + run_id +
+              " (" + total_fetched + "-" + next_run_max + " of " + filterValues.length + ").";
+          tableau.reportProgress(status_msg);
+        }
 
-        tableau.reportProgress(status_msg);
-        run_data = getValue(ts_url + "/runs/" + run.id);
+        run_data = getValue(ts_url + "/runs/" + run_id);
 
-        var date_str = run_data.run.end_time;
-        var run_date = new Date(date_str);
-        var derived_run_data = {
-          "machine_name": machine_name,
-          "run_id": run.id,
-          "run_order": run[run.order_by],
-          "run_date": run_date
-        };
         run_data.tests.forEach(function (element) {
           element.test_name = element.name;
           delete element.name;
-          var data = Object.assign({}, derived_run_data, element);
-          tableData.push(data);
+          tableData.push(element);
 
         });
+        if (tableData.length > submission_batch_size) {
+          table.appendRows(tableData);
+          tableData.length = 0;  // Drop the submitted rows.
+        }
+        total_fetched = total_fetched + 1;
         run_data = null;
-      });
-
+      }
       table.appendRows(tableData);
-
-    });
+    } else {
+      throw new Error("Unexpected table id " + table.tableInfo.id)
+    }
     doneCallback();
   };
 
