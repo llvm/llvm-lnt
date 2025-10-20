ed3884697e84b3dd9929b73ee20f6b5107d5fff1
Luke Lau <luke@igalia.com>
2025-10-20 03:52:11 +0000

    Persist LNT instance and config in docker-compose.yml
    Otherwise the configuration is recreated everytime the image is launched and we lose all the profiles etc.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,20 +3,20 @@
 DB_PATH=${DB_ENGINE:-postgresql}://${DB_USER:-lntuser}:${DB_PWD:?}@${DB_HOST:?}
 DB_BASE=${DB_BASE:-lnt}
 
-if [ ! -r /etc/lnt.cfg ]; then
+if [ ! -r /etc/lnt/lnt.cfg ]; then
   DB_BASE_PATH="${DB_PATH}/${DB_BASE}" wait_db
-  lnt create /var/lib/lnt \
-	  --config /etc/lnt.cfg \
+  lnt create /var/lib/lnt/instance \
+	  --config /etc/lnt/lnt.cfg \
 	  --wsgi lnt_wsgi.py \
 	  --tmp-dir /tmp/lnt \
 	  --db-dir "${DB_PATH}" \
 	  --default-db "${DB_BASE}"
   if [ -n "${LNT_AUTH_TOKEN:-}" ]; then
-    sed -i "s/# \(api_auth_token =\).*/\1 '${LNT_AUTH_TOKEN}'/" /etc/lnt.cfg
+    sed -i "s/# \(api_auth_token =\).*/\1 '${LNT_AUTH_TOKEN}'/" /etc/lnt/lnt.cfg
   fi
 fi
 
-cd /var/lib/lnt
+cd /var/lib/lnt/instance
 exec gunicorn lnt_wsgi:application \
 	--bind 0.0.0.0:8000 \
 	--workers 8 \
