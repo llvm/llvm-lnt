bffaf2c1889000682fb332cbf375c5e301799d5b
Louis Dionne <ldionne.2@gmail.com>
2026-02-09 13:56:03 -0500

    Add a way to pass additional machine information in lnt importreport

--- lnt/lnttool/import_report.py
+++ lnt/lnttool/import_report.py
@@ -12,12 +12,17 @@ import click
               "Ex: a svn revision, or timestamp.")
 @click.option("--machine", required=True,
               help="the name of the machine to submit under")
-@click.option("--run-info", multiple=True, type=str,
+@click.option("--run-info", "run_info", multiple=True, type=str,
               help="Optional additional run information to include in the submission. "
                    "If provided, this must be a key-value pair separated by '='. This "
                    "argument may be repeated multiple times to provide multiple keys "
                    "and values in the run information.")
-def action_importreport(input, output, suite, order, machine, run_info):
+@click.option("--machine-info", "machine_info", multiple=True, type=str,
+              help="Optional additional machine information to include in the submission. "
+                   "If provided, this must be a key-value pair separated by '='. This "
+                   "argument may be repeated multiple times to provide multiple keys "
+                   "and values in the machine information.")
+def action_importreport(input, output, suite, order, machine, run_info, machine_info):
     """Import simple data into LNT. This takes a space separated
     key value file and creates an LNT report file, which can be submitted to
     an LNT server. Example input file:
@@ -33,22 +38,26 @@ def action_importreport(input, output, suite, order, machine, run_info):
     import lnt.testing
     import os
 
-    machine = lnt.testing.Machine(machine, report_version=2)
-
-    parsed_info = {}
+    # Build the run with any additional info
+    run_info_dict = {}
     for s in run_info:
         if '=' not in s:
             raise click.BadParameter(f"--run-info must be in 'key=value' format, got: {s}")
         k, v = s.split('=', 1)  # Split only on the first '=' in case there are several in the string
-        parsed_info[k] = v
-    run_info = parsed_info
-    run_info.update({'llvm_project_revision': order})
-
+        run_info_dict[k] = v
+    run_info_dict.update({'llvm_project_revision': order})
     ctime = os.path.getctime(input.name)
     mtime = os.path.getmtime(input.name)
-    run = lnt.testing.Run(start_time=ctime, end_time=mtime,
-                          info=run_info,
-                          report_version=2)
+    run = lnt.testing.Run(start_time=ctime, end_time=mtime, info=run_info_dict, report_version=2)
+
+    # Build the machine with any additional info
+    machine_info_dict = {}
+    for s in machine_info:
+        if '=' not in s:
+            raise click.BadParameter(f"--machine-info must be in 'key=value' format, got: {s}")
+        k, v = s.split('=', 1)  # Split only on the first '=' in case there are several in the string
+        machine_info_dict[k] = v
+    machine = lnt.testing.Machine(machine, info=machine_info_dict, report_version=2)
 
     tests = {}  # name => lnt.testing.Test
     for line in input.readlines():
