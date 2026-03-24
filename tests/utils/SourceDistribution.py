f9fb2445a9b27f4ad8fb7dfa10fc34891a4b69c0
Louis Dionne <ldionne.2@gmail.com>
2026-03-24 08:26:48 -0400

    Remove outdated source distribution files
    The test was never running, and *what* it tests is actually rendered
    moot by the migration to pyproject.toml and setuptools-scm, which
    ensure that all necessary files are included in the source distribution.

--- tests/utils/SourceDistribution.py
+++
@@ -1,9 +0,0 @@
-# Check that our source distribution is "clean" (includes everything we need it
-# to).
-#
-# RUN: %{src_root}/utils/check-sdist %{src_root}
-#
-# Disable this test by default, it isn't particularly slow but it is a tad
-# invasive.
-#
-# REQUIRES: long
